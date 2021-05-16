afdd8964e2048eecb83005419a1190fa88320000
jainnikhil30 <jainnikhil30@gmail.com>
2021-05-16 18:19:47 +0530

    Update connection.php

--- login/connection.php
+++ login/connection.php
@@ -1,7 +1,7 @@
 <?php      
     $host = "sql6.freemysqlhosting.net";  
     $user = "sql6411807";  
-    $password = 'kyRVg9HSXx';  
+    $password = 'password';  
     $db_name = "sql6411807";  
       
     $con = mysqli_connect($host, $user, $password, $db_name);  
